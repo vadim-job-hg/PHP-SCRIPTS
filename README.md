a3fb0c54441bd53d9874a2b7fb79f9c14bf56197
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-03-29 18:12:33 +0300

    #simple #cron #wodpress

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Here you can see just the seemple php scripts that can be used on can be not used
 * cron.php - just cron
 * wp-real-cron.php - also cron for wordpress
+* paginator.php - array PaginatorBuirlder::buildPaginationArray($current_page = 1, $page_count = 1, $page_strike_length = 2)  - will return the array for pagination
+### examples
+* * // make examples
---
+++ paginator.php
@@ -0,0 +1,25 @@
+<?php
+class PaginatorBuirlde{
+
+    static function buildPaginationArray($current_page = 1, $page_count = 1, $page_strike_length = 2){
+        $paginator_array = [];
+        for($index = 1; $index<=$page_count; $index++){
+            if($index<=$page_strike_length || $index>($page_count-$page_strike_length)
+                || ($index>($current_page - $page_strike_length) && $index<($current_page + $page_strike_length))) {
+                if ($index == $current_page)
+                    $paginator_array[] = array('type' => 'current', 'num' => $index);
+                else
+                    $paginator_array[] = array('type' => 'page', 'num' => $index);
+            }
+            elseif($index<$current_page - $page_strike_length){
+                $paginator_array[] = array('type'=>'delimer');
+                $index= $current_page - $page_strike_length;
+            } else {
+                $paginator_array[] = array('type'=>'delimer');
+                $index = $page_count - $page_strike_length;
+            }
+
+        }
+        return $paginator_array;
+    }
+}
